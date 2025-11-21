907fa159a3089b9a9a3f4186aaa8d920faa35fe9
alexxxsand <156219433+alexxxsand@users.noreply.github.com>
2025-11-20 21:46:16 -0500

    Create Pantry.jsx

---
+++ src/pages/Pantry.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function Pantry() {
+return (
+<div className="page">
+<Header onMenu={() => {}} />
+<main className="card">
+<h2>Pantry</h2>
+<p>List of scanned items will be shown here.</p>
+</main>
+</div>
+)
+}
